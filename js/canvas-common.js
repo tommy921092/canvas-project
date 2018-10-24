325c8da75083c47402d4b0f223ecd62ab0f4862d
Tom Chan <ittomchan@gmail.com>
2018-10-24 14:44:18 +0800

    1024 1226 Tom with download function

--- js/canvas-common.js
+++ js/canvas-common.js
@@ -69,3 +69,11 @@ class PaintFunction{
     }
           
 }
+
+function download(){
+    var download = document.getElementById("download");
+    var image = document.getElementById("canvas-real").toDataURL("image/png")
+                // .replace("image/png", "image/octet-stream");
+                // console.log(image)
+    download.setAttribute("href", image);
+}
